6545dfcf72376ab7580d735502ff3136b5b24888
Bryan Shay <forgetfulmind@gmail.com>
2021-02-23 15:01:05 -0500

    additional manager functionality

--- brains.js
+++ brains.js
@@ -6,17 +6,18 @@ const { title } = require('process');
 const connection = mysql.createConnection({
     host: 'localhost',
   
-    // Your port; if not 3306
+    // port
     port: 3306,
   
-    // Your username
+    // username
     user: 'root',
   
-    // Be sure to update with your own MySQL password!
+    // Be sure to export your password as DB_password
     password: process.env.DB_password,
     database: 'employee_tracker_db',
   });
 
+//main menu
 const rootChoice = ()=>{
   inquirer
   .prompt([
@@ -25,6 +26,8 @@ const rootChoice = ()=>{
           choices:[
               'View All Employees',
               'Add Employee',
+              `Modify an Employee's Manager`,
+              `View Employees by Manager`,
               'View All Departments',
               'Add Department',
               'View All Roles',
@@ -54,6 +57,12 @@ const rootChoice = ()=>{
         break;
         case "Add Role":
             addRole()
+        break;    
+        case `Modify an Employee's Manager`:
+            modEmployee()
+        break;  
+        case `View Employees by Manager`:
+            viewByManager()
         break;       
         case "Exit":
             console.log("okay bye bye")
@@ -63,8 +72,10 @@ const rootChoice = ()=>{
     }
   })
 }
+
+//view all employees
   const viewEmployees = ()=>{
-    connection.query('SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, employees.manager_id, departments.dept_name FROM employees INNER JOIN roles ON employees.role_id=roles.id INNER JOIN departments ON roles.department_id=departments.id', (err, res) => {
+    connection.query('SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, employees.manager_id, departments.dept_name FROM employees INNER JOIN roles ON employees.role_id=roles.id INNER JOIN departments ON roles.department_id=departments.id ORDER BY employees.id', (err, res) => {
         if (err) throw err;
         // Log all results of the SELECT statement
         console.table(res);
@@ -72,6 +83,7 @@ const rootChoice = ()=>{
       });
   }
 
+//view all departments
   const viewDepartments = ()=>{
     connection.query('SELECT * FROM departments', (err, res) => {
         if (err) throw err;
@@ -81,6 +93,7 @@ const rootChoice = ()=>{
       });
   }
 
+//view all roles  
   const viewRoles = ()=>{
     connection.query('SELECT roles.id, roles.title, roles.salary, departments.dept_name FROM roles INNER JOIN departments ON roles.department_id=departments.id', (err, res) => {
         if (err) throw err;
@@ -90,6 +103,7 @@ const rootChoice = ()=>{
       });
   }
 
+// add an employee
   const addEmployee = ()=>{
     connection.query('SELECT roles.title FROM roles', (err,res)=>{
         if (err) throw err;
@@ -187,7 +201,6 @@ const rootChoice = ()=>{
                 (err, res) => {
                   if (err) throw err;
                   console.log(`${res.affectedRows} employee created!\n`);
-                  // Call updateProduct AFTER the INSERT completes
                   rootChoice();
                 }
             )
@@ -199,6 +212,7 @@ const rootChoice = ()=>{
    })
   }
 
+// add a department to the list 
   const addDepartment = ()=>{
     inquirer
     .prompt([
@@ -216,14 +230,13 @@ const rootChoice = ()=>{
                 (err, res) => {
                   if (err) throw err;
                   console.log(`${res.affectedRows} department created!\n`);
-                  // Call updateProduct AFTER the INSERT completes
                   rootChoice();
                 }
            )
     })
   }
 
-
+//add a role to the list 
   const addRole = ()=>{
     inquirer
     .prompt([
@@ -282,7 +295,6 @@ const rootChoice = ()=>{
                 (err, res) => {
                   if (err) throw err;
                   console.log(`${res.affectedRows} department created!\n`);
-                  // Call updateProduct AFTER the INSERT completes
                   rootChoice();
                 }
               )
@@ -290,6 +302,171 @@ const rootChoice = ()=>{
     })
   } 
 
+//modify an employee's manager  
+  const modEmployee = ()=>{
+    inquirer
+    .prompt([
+        {
+            name: 'employeeChoice',
+            type: 'rawlist',
+            choices(){
+                return new Promise((resolve, reject)=>{
+                    connection.query('SELECT employees.first_name, employees.last_name FROM employees', (err,res)=>{
+                        //console.log(res)
+                        let arr = [];
+                        res.forEach(({first_name, last_name}) => {
+
+                           arr.push(`${first_name} ${last_name}`)
+                        });
+                        //console.log(arr)
+                        resolve(arr);
+                })
+                })
+            },
+            message: 'Which Employee would you like to modify? ',
+        },
+        {
+            name: 'managerChoice',
+            type: 'rawlist',
+            choices(){
+                return new Promise((resolve, reject)=>{
+                    connection.query('SELECT employees.first_name, employees.last_name FROM employees', (err,res)=>{
+                        //console.log(res)
+                        let arr = ["No Manager"];
+                        res.forEach(({first_name, last_name}) => {
+
+                           arr.push(`${first_name} ${last_name}`)
+                        });
+                        //console.log(arr)
+                        resolve(arr);
+                })
+                })
+            },
+            message: `Who is this employee's manager?`,
+        },
+    ])
+    .then((response)=>{
+        let managerChoice = response.managerChoice.split(" ")
+        let managerID;
+        let employeeChoice = response.employeeChoice.split(" ")
+         getManager()
+        function getManager(){
+            new Promise((resolve, reject)=>{
+                if(response.managerChoice !== "No Manager"){
+                    connection.query('SELECT employees.id FROM employees WHERE ? AND ?',
+                    [
+                        {
+                            first_name: managerChoice[0],
+                        }, 
+                        {
+                            last_name: managerChoice[1],
+                        },
+                ],
+                    (err,res)=>{
+                        managerID = res[0].id
+                        resolve(updateManager());
+                   })
+                }else{resolve(updateManager())}
+          })
+        }
+
+        function updateManager(){
+            
+            connection.query(
+                'UPDATE employees SET ? WHERE ? AND ?',
+                [
+                    {
+                        manager_id: managerID
+                    },
+                    {   first_name: employeeChoice[0],  
+                    },
+                    {
+                        last_name: employeeChoice[1],
+                    },
+                ],
+                (err, res) => {
+                  if (err) throw err;
+                  console.log(`${res.affectedRows} employee's manager updated!\n`);
+                  rootChoice();
+                }
+            )
+        }
+  
+    })
+  }
+
+//view employees by manager
+  const viewByManager = ()=>{
+    inquirer
+    .prompt([
+        {
+            name: 'managerView',
+            type: 'rawlist',
+            choices(){
+                
+                return new Promise((resolve, reject)=>{
+                    connection.query('SELECT employees.first_name, employees.last_name FROM employees', (err,res)=>{
+                        //console.log(res)
+                        let arr = [];
+                        res.forEach(({first_name, last_name}) => {
+
+                           arr.push(`${first_name} ${last_name}`)
+                        });
+                        //console.log(arr)
+                        resolve(arr);
+                })
+                })
+            },
+            message: `Which Manager's Employee list would you like to view?`,
+        },
+    ])
+    .then((response)=>{
+        let managerView = response.managerView.split(" ")
+        let managerID;
+         getManager()
+
+        function getManager(){
+            new Promise((resolve, reject)=>{
+                    connection.query('SELECT employees.id FROM employees WHERE ? AND ?',
+                    [
+                        {
+                            first_name: managerView[0],
+                        }, 
+                        {
+                            last_name: managerView[1],
+                        },
+                ],
+                    (err,res)=>{
+                        managerID = res[0].id
+                        resolve(viewChosen());
+                   })
+          })
+        }
+
+//select employees where ID is manager ID 
+        function viewChosen(){
+            connection.query(
+                'Select * FROM employees WHERE ?',
+                [
+                    {
+                        manager_id: managerID
+                    },
+
+                ],
+                (err, res) => {
+                  if (err) throw err;
+                  if(!res[0]){
+                    console.log(`|| This selection currently has no employees assigned ||`)
+                  }else{
+                    console.table(res);
+                  }
+                  rootChoice();
+                }
+            )
+        }
+  
+    })
+  }
 
 
   //init
